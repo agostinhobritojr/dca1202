553ba532eeccc1531b54fea48550bc0b20659105
agostinhobritojr <agostinhobritojr@gmail.com>
2017-04-17 08:24:06 -0300

    templates

--- templateclass/main.cpp
+++ templateclass/main.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main()
 {
-  Vetor<int> v1;
+  Vetor<Matriz> v1, v4;
   Vetor<float> v2;
   Vetor<double> v3;
 
-  v1.setX(4);
+  v1.setX(4.6);
   v2.setX(5);
   v3.setX(6);
+  v4 = v1.copia();
 
   cout << v1.getX() << endl;
+  cout << v4.getX() << endl;
   cout << v2.getX() << endl;
   cout << v3.getX() << endl;
 
