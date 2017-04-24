7c7f139692782e89b05c72262a0f94bf3adf3f65
agostinhobritojr <agostinhobritojr@gmail.com>
2017-04-24 08:41:15 -0300

    arquivos

--- streams/main.cpp
+++ streams/main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(){
-  ofstream saida;
-  ifstream entrada;
+  ofstream saida; // arquivo de SAIDA
+  ifstream entrada; // arquivo de ENTRADA
   float x;
 
   entrada.open("/home/ambj/entrada.txt");
@@ -13,6 +13,25 @@ int main(){
     entrada >> x;
     cout << "x = " << x << endl;
     entrada.close();
+  }
+
+  char c;
+  entrada.open("/home/ambj/entrada.txt");
+  saida.open("/home/ambj/clone.txt");
+  if(entrada.is_open()){
+    while(entrada.get(c)){
+      cout << "leu: " << c << endl;
+      saida.put(c);
+    }
+    entrada.close();
+    saida.close();
+  }
+
+  entrada.open("/home/ambj/nome.txt");
+  char nome[40];
+  if(entrada.is_open()){
+    entrada.getline(nome,40);
+    cout << nome << endl;
   }
 
   saida.open("/home/ambj/saida.txt");
