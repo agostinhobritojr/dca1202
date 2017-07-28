92b60d4b1cbd9a08052cec8bff009038259d6938
agostinhobritojr <agostinhobritojr@gmail.com>
2017-07-28 08:37:41 -0300

    revisao

--- revisaoc/main.c
+++ revisaoc/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
-int main(void)
-{
+#define LIMIARCRITICO 38
+#define CTE_AGOSTINHO 234.6789434
+int main(int argc, char* argv[]){
+  int x, y;
+
+
   printf("Hello World!\n");
+  printf("valor= %d e alo = [%5.2f]\n", 3, 4444.34);
+
+
+  if(a == LIMIARCRITICO){
+    exec("explode");
+  }
+  else{
+    exec("queima");
+  }
+
+
+  x = CTE_AGOSTINHO;
+
+  x = 3.1415;
+//  a = sin(x*ln(x/3.14));
+
+  scanf("%d %d", &x, &y);
+  // a linha de baixo eh um printf
+  printf("x = %d; y = %d\n", x, y);
+
   return 0;
 }
-
