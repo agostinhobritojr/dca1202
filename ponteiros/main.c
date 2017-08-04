4ab107daf18640aa6ad6cb737d449c321d1fdcfe
agostinhobritojr <agostinhobritojr@gmail.com>
2017-08-04 08:33:59 -0300

    ponteiros2

--- ponteiros/main.c
+++ ponteiros/main.c
@@ -17,9 +17,27 @@ void troca(int *a, int *b){
   printf("troca: a = %d; b = %d\n", *a, *b);
 }
 
+void ordena(int *a, int *b, int *c){
+  int tmp;
+  if(*a > *b){
+    troca(a, b);
+  }
+  if(*a > *c){
+    troca(a, c);
+  }
+  if(*b > *c){
+    troca(b, c);
+  }
+}
+
 int main(void){
-  int a, b;
-  a = 3; b = 4;
+  int a, b, c;
+  a = 4; b = 3; c = 2;
+
+  ordena(&a, &b, &c);
+  printf("\n\na, b, c = %d, %d, %d\n\n", a,b,c);
+
+
   printf("endereco de a: %p\n",&a);
   printf("endereco de b: %p\n",&b);
   printf("main: a = %d; b = %d\n", a, b);
