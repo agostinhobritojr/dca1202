a0e9066e1974d5b30ee701698d0b880fefac702b
agostinhobritojr <agostinhobritojr@gmail.com>
2018-03-21 07:46:46 -0300

    refer

--- referencias/main.cpp
+++ referencias/main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-int main()
-{
-  cout << "Hello World!" << endl;
+void f(int &x){
+  cout << "endx = " << &x << endl;
+  x = x + 1;
+}
+
+int main(){
+  int a = 3;
+  f(a);
+  cout << "endx = " << &a << endl;
+  cout << "x = " << a << endl;
   return 0;
 }
