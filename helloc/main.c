b09fcc3044ca3c763ecea056c2d230e6ffbf9439
agostinhobritojr <agostinhobritojr@gmail.com>
2019-02-20 08:29:42 -0300

    bitbyte

--- helloc/main.c
+++ helloc/main.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+struct alo{
+  char x;
+  char w;
+  int y;
+  float z;
+};
 
 int main(void){
   unsigned char x;
+  char nome[10];  long int y;
+  struct alo teste;
+
+  teste.x = 3;
+  teste.y = 4;
+  teste.z = 0.67;
+
+  printf("A variavel teste "
+         "ocupa %d bytes\n",
+         sizeof(teste));
+
+  if(-1){
+    printf("alo");
+  }
+
+//  strcpy(nome, "mariamaria");
+  /*
+
+  fgets(nome, 10, stdin);
+  puts(nome);
+  nome[4]=0;
+  puts(nome);
+
+  printf("%d, %d %c\n",
+         4,
+         nome[4],
+         nome[4]);
+
   x = 255;
-  printf("x = %d\n", x);
-  x = x+1;
-  printf("x = %d\n", x);
-  x = x-1;
-  printf("x = %d\n", x);
+  printf("tam = %d\n",
+         sizeof(y));
+  printf("tam = %d\n",
+         sizeof(long long int));
+*/
   return 0;
 }
