8b285a341f12ddccb691562a84318052772bbd1c
agostinhobritojr <agostinhobritojr@gmail.com>
2019-04-22 07:38:21 -0300

    streams

--- fluxos/main.cpp
+++ fluxos/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(){
   ofstream fout;
-
+  ifstream fin;
+  char nome[20];
+  string alo;
+  int idade;
   // "c:/usuarios/jose\ antonio/alo.txt"
   fout.open("/home/ambj/alo.txt");
   if(fout.is_open()){
@@ -15,9 +19,20 @@ int main(){
     cout << "arquivo nao foi aberto\n";
     exit(1);
   }
-  fout << "Agostinho" << 34 << endl;
-
+  fout << "Agostinho Brito|" << 34 << endl;
   fout.close();
+  // alo.txt precisa existir e
+  // o usuario ter permissoes de acesso
+  fin.open("/home/ambj/alo.txt");
+  if(!fin.is_open()){
+    exit(0);
+  }
 
+  getline(fin, alo);
+  cout << alo << endl;
+//  fin >> nome >> idade;
+//  cout << nome << idade << endl;
   return 0;
 }
+
+
