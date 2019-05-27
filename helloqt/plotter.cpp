477db56815c300231c981f679323915053c587c1
agostinhobritojr <agostinhobritojr@gmail.com>
2019-05-27 08:30:53 -0300

    helloqt

--- helloqt/plotter.cpp
+++ helloqt/plotter.cpp
@@ -2,6 +2,7 @@
 #include <QPainter>
 #include <QPen>
 #include <QBrush>
+#include <cmath>
 
 Plotter::Plotter(QWidget *parent) :
     QWidget(parent)
@@ -14,6 +15,9 @@ void Plotter::paintEvent(QPaintEvent *event)
   QPainter p(this);
   QPen pen;
   QBrush brush;
+
+  // habilita algoritmos de anti-serrilhamento
+  p.setRenderHint(QPainter::Antialiasing);
 
   // preparando a caneta
   // R, G, B
@@ -29,6 +33,29 @@ void Plotter::paintEvent(QPaintEvent *event)
   p.setBrush(brush);
 
   p.drawRect(0,0,width(),height());
+
+  pen.setColor(QColor(0,0,255));
+  pen.setStyle(Qt::DashLine);
+  p.setPen(pen);
+
+  p.drawLine(0,height()/2, width(), height()/2);
+
+  // seno desenhado com vermelho
+  pen.setColor(QColor(255,0,0));
+  pen.setWidth(1);
+  pen.setStyle(Qt::SolidLine);
+  p.setPen(pen);
+
+  int p1x, p1y, p2x, p2y;
+  p1x=0;
+  p1y=height()/2;
+  for(int t=0; t<width(); t++){
+    p2x = t;
+    p2y = height()/2 - height()/2*
+        sin(2*3.1415*t/width());
+    p.drawLine(p1x, p1y, p2x, p2y);
+    p1x = p2x; p1y = p2y;
+  }
 }
 
 
