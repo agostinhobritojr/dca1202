a91f2c12c18647f34eb0f6e51e64eea0e76f14d9
agostinhobritojr <agostinhobritojr@gmail.com>
2019-08-02 08:32:39 -0300

    troca

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-  printf("Hello World!\n");
+int main(int argc, char* argv[]){
+  struct aluno{
+    char nome[40];
+    int matricula;
+  };
+
+  struct abc{
+    char a;
+    int c;
+    char b;
+  };
+
+  unsigned char x;
+
+  struct aluno joao;
+  struct abc alo;
+
+  printf("sizeof alo = %d\n",
+         sizeof (alo));
+
+  strcpy(joao.nome, "Joao maria");
+  joao.matricula=20193045;
+
+  x = -1;
+  printf("x = %d\n", x);
+  x--;
+  printf("x = %d\n", x);
+  x++;
+  printf("x = %d\n", x);
+
+  //  char nome[10];
+  //  gets(nome);
+  unsigned long int y;
+  y = 1-3+1;
+
+  printf("mem = %u\n", y);
+  /*  for(i=0; i<argc; i++){
+    printf("%s\n", argv[i]);
+  }*/
+
   return 0;
 }
+
+
+
+
+
+
+
