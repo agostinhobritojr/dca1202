f2537868de900864cc62d66ffd8ff64efcfb13ab
Victor Costa <39657648+VictorCosta101@users.noreply.github.com>
2019-09-16 08:31:03 -0300

    Update main.cpp

--- fluxos/main.cpp
+++ fluxos/main.cpp
@@ -1,39 +1,66 @@
-#include <iostream>
+#include <sculptor.h>
 #include <fstream>
-#include <cstdlib>
-#include <iomanip>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-  ofstream fout;
-  ifstream fin;
-  int nl, nc, x;
-  fin.open("/home/ambj/entrada.txt");
-  if(!fin.is_open()){
-    exit(0);
-  }
-  // abrir o arquivo
-  fout.open("/home/ambj/saida.txt");
-  // verificar se estah aberto
-  if(!fout.is_open()){
-    exit(0);
-  }
 
-  fin >> nl >> nc;
-  fout << nl << " " << nc << endl;
-  for (int i=0; i<nl; i++) {
-    for (int j=0; j<nc; j++) {
-      fin >> x;
-      fout << setw(3) << x << " ";
+
+Sculptor::Sculptor(int _nx, int _ny, int _nz)
+{
+
+    Voxel ***v = new Voxel**[_nx];
+    for (int i = 0; i < _nz; ++i) {
+      v[i] = new Voxel*[_ny];
+      for (int j = 0; j < _ny; ++j)
+        v[i][j] = new Voxel[_nz];
     }
-    fout << endl;
-  }
+}
 
-  fin.close();
-  fout.close();
+Sculptor::~Sculptor()
+{
 
-  return 0;
+    for(int i = 0; i < nx; i++)
+    {
+        for(int j = 0; j < ny; j++)
+        {
+            delete[] v[i][j];
+
+            delete[] v[i];
+        }
+
+    }
+    delete[] v;
+}
+
+void Sculptor::setColor(float r, float g, float b, float alpha)
+{
+    this-> r = r;
+    this-> g = g;
+    a = alpha;
+}
+
+void Sculptor::putVoxel(int x, int y, int z)
+{
+    if( x <= nx && y <= ny && z <= nz )
+    {
+    v[x][y][z].isOn=true
+    v[x][y][z].r=r ;
+    v[x][y][z].g=g ;
+    v[x][y][z].b=b ;
+    v[x][y][z].a=a ;
+
+    }
+}
+
+void Sculptor::cutVoxel(int x, int y, int z)
+{
+    v[x][y][z].isOn=false;
+
+}
+
+void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1)
+{
+    
 }
 
 
