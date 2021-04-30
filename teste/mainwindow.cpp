a13c0a623d1107ca609b8123c5f525639bd08223
Agostinho Brito <agostinhobritojr@gmail.com>
2021-04-30 07:52:30 -0300

    teste

---
+++ teste/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  /*QObject::connect(ui->horizontalSlider,
+                   SIGNAL(valueChanged(int)),
+                   ui->lcdNumber,
+                   SLOT(display(int)));*/
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
--- testea/testea.pro
+++ testea/testea.pro
@@ -4,8 +4,4 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
-        main.cpp \
-        vetor.cpp
-
-HEADERS += \
-  vetor.h
+        main.cpp
