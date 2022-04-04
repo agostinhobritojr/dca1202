f85a11f147b254f3a9304381697d9daf58db7932
Agostinho Brito <agostinhobritojr@gmail.com>
2022-04-04 07:49:56 -0300

    bitabit

--- bitbyte/main.c
+++ bitbyte/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
+void printbyte(unsigned char c){
+  unsigned char i;
+  unsigned char and;
+  for(i=128; i>0; i=i>>1){
+    and = i & c;
+    if(and){
+      printf("1");
+    }
+    else{
+      printf("0");
+    }
+  }
+  printf("\n");
+}
+
 int main(){
   int i;
   unsigned int x;
   unsigned char *px;
+
+  unsigned char c;
+  printbyte(12);
+
+
+
 
   x = 0;
   px = &x;
