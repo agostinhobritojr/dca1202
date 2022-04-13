7ed3c909cee5cb03a49a7e58a948e18136ce8112
Agostinho Brito <agostinhobritojr@gmail.com>
2022-04-13 07:43:07 -0300

    alo

---
+++ matrizes/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int **x;
+  int nl, nc;
+  printf("digite nl: ");
+  scanf("%d", &nl);
+  printf("digite nc: ");
+  scanf("%d", &nc);
+
+  x = malloc(n * sizeof (int));
+
+  free(x);
+  return 0;
+}
