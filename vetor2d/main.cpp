14fe62a49122885fa7d737aa6a596133140fdfc8
Agostinho Brito <agostinhobritojr@gmail.com>
2022-05-04 08:36:26 -0300

    modular

---
+++ vetor2d/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vetor2d.h"
+
+int main(void){
+  Vetor2d v;
+  v.setX(3); std::cout << v.getX() ;
+}
+
---
+++ vetor2d/vetor2d.cpp
@@ -0,0 +1,9 @@
+#include "vetor2d.h"
+
+void Vetor2d::setX(float x_){
+  x = x_;
+}
+
+float Vetor2d::getX(){
+  return x;
+}
