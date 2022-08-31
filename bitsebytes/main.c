85a3d04ba729e07db7ec5eb48a8acc2eedfce8d3
Agostinho Brito <agostinhobritojr@gmail.com>
2022-08-31 08:34:48 -0300

    .

--- bitsebytes/main.c
+++ bitsebytes/main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 void printbits(unsigned char c){
-
+  unsigned char bit;
+  for(bit=128; bit !=0 ;bit>>=1){
+    if(c & bit)  printf("1");
+    else  printf("0");
+  }
 }
 
 int main(){
+  int x;
+  x = 5020312;
   unsigned char c;
+  unsigned char *pc;
   c = 37;
-  printbits(c);
-
+  pc = (unsigned char*) &x;
+ // pc = pc+1;
+ // printbits( *(pc+3) ); // byte 3
+//  printbits( *(pc+2) ); // byte 2
+//  printbits( *(pc+1) ); // byte 1
+//  printbits(*pc); // byte 0
+  printbits( pc[3] );
+  printbits( pc[2] );
+  printbits( pc[1] );
+  printbits( pc[0] );
+  printf("\n");
   return 0;
 }
