004965556d77aca1ce03421a505fd1ceac12a81d
Agostinho Brito <agostinhobritojr@gmail.com>
2022-09-26 08:08:36 -0300

    vetor2d

---
+++ maketest/vetor2d.cpp
@@ -0,0 +1,12 @@
+#include "vetor2d.h"
+
+// nome_classe
+void Vetor2d::setX(float x_){
+  //        ^^ operador de escopo
+  x = x_;
+}
+
+float Vetor2d::getX(){
+  return x;
+}
+
---
+++ vetor/vetor2d.h
@@ -0,0 +1,12 @@
+#ifndef VETOR2D_H
+#define VETOR2D_H
+
+class Vetor2d{
+private:
+  float x, y;
+public:
+  void setX(float x_);
+  float getX();
+};
+
+#endif // VETOR2D_H
