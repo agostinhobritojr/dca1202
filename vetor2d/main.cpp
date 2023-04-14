82792f9d36bacd922797430d6560c8215353b3c9
Agostinho Brito <agostinhobritojr@gmail.com>
2023-04-14 08:35:47 -0300

    .

--- vetor2d/main.cpp
+++ vetor2d/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath> // sqrt
 
 class Vetor2d{
 private:
@@ -11,12 +12,29 @@ public:
   float getX(){
     return x;
   }
+  void setY(float y_){
+    y = y_;
+  }
+  float getY(){
+    return y;
+  }
+  void print(void){
+    std::cout << "(" << x << "," << y << ")";
+  }
+  float norma(void){
+    return std::sqrt(x*x + y*y);
+  }
 };
 
 int main(void){
   Vetor2d v;
-  v.x = 4;
-  v.setX(3); std::cout << v.getX();
+//  v.x = 4;
+  v.setX(3); v.setY(4);
+  v.print();
+  std::cout << "\n";
+  std::cout << v.norma() << "\n";
+
+//  std::cout << v.getX();
 }
 
 
