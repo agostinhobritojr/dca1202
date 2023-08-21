34292f23e792bc0ff9d2212cdbc397f5c7d20032
Agostinho <agostinhobritojr@gmail.com>
2023-08-21 07:59:25 -0300

    troca

--- troca/troca.c
+++ troca/troca.c
@@ -2,16 +2,27 @@
 
 int z=0;
 
-void troca(int a, int b){
+void ordena(int *a, int *b, int *c){
+  
+}
+
+void troca(int *a, int *b){
   static int tmp=0;
-  printf("a = %d; b = %d\n", a, b);
-  tmp=a; a=b; b=tmp;
-  printf("a = %d; b = %d\n", a, b);
+  printf("tmp -> %p\n", &tmp);
+  printf("z   -> %p\n", &z);
+  printf("a   -> %p\n", &a);
+  printf("b   -> %p\n", &b);
+  printf("\n");
+  printf("*a = %d; *b = %d\n", *a, *b);
+  tmp = *a; 
+  *a = *b; 
+  *b = tmp;
+  printf("*a = %d; *b = %d\n", *a, *b);
 }
 int main(void){
   int x=3, y=4;
   printf("x = %d; y = %d\n", x, y);
-  troca(x, y);
+  troca(&x, &y);
   printf("x = %d; y = %d\n", x, y);
   return 0;
 }
