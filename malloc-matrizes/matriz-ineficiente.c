0ba916b8fcf16837915d7a210d5e531cdb41dd9d
Agostinho <agostinhobritojr@gmail.com>
2023-08-28 08:33:27 -0300

    matrizes

--- malloc-matrizes/matriz-ineficiente.c
+++ malloc-matrizes/matriz-ineficiente.c
@@ -3,17 +3,28 @@
 
 int main(void){
   int nl=4, nc=5, i, j;
-  int **x, **y;
+  int **x, **y; // y = x (transposta)
   x = (int**) malloc(nl * sizeof(int*));
   for(i=0; i<nl; i++){
     x[i] = (int*) malloc(nc * sizeof(int));
   }
+
+  y = (int**) malloc(nl * sizeof(int*));
+  for(i=0; i<nl; i++){
+    y[i] = (int*) malloc(nc * sizeof(int));
+  }
+
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
       x[i][j] = i+j;
     }
   }
-  y = x;
+
+  for(i=0; i<nl; i++){
+    for(j=0; j<nc; j++){
+      y[i][j] = x[i][j];
+    }
+  }
 
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
@@ -21,9 +32,15 @@ int main(void){
     }
     printf("\n");
   }
-
+  // libera blocos com as linhas
   for(i=0; i<nl; i++){
     free (x[i]);
   }
+  // libera bloco de ponteiros auxiliares
   free (x);
+
+  for(i=0; i<nl; i++){
+    free (y[i]);
+  }
+  free (y);
 }
