5db79c0e771757a9a2f99315c52620fd25f3de26
Agostinho <agostinhobritojr@gmail.com>
2023-10-06 07:38:17 -0300

    operadores

---
+++ operadores/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "vetor2d.h"
+
+//using std::cout;
+
+int main(){
+    Vetor2d v1(1,2), v2(3,4), v3;
+    
+    v3 = v2 + v1;
+   // v3 = v1.operator+(v2);
+    v3.print(); std::cout << std::endl;
+    v3 = v3*4;
+    v3.print(); std::cout << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operadores/vetor2d.h
@@ -0,0 +1,47 @@
+#ifndef VETOR2D_H
+#define VETOR2D_H
+
+class Vetor2d {
+private:
+    // propriedades
+    float y;
+    float x;
+public:
+    // construtoRES
+    // construtor padraao
+ //   Vetor2d();
+    // construtorES com argumentos
+    Vetor2d(float x_=0, float y_=0);
+//    Vetor2d(float xy);
+    // construtor de copia
+    Vetor2d(const Vetor2d &copia);
+    // destrutor
+    ~Vetor2d();
+    // metodos
+    void setX(float x);
+    float getX(void);
+    void setY(float x_);
+    float getY(void);
+    void print();
+
+    float norma();
+    float angulo();
+    Vetor2d unitario();
+    Vetor2d soma(Vetor2d v1);
+    Vetor2d operator + (Vetor2d v1);
+    Vetor2d operator*(float a);
+    // sobrecargas do metodo produto
+    // produto POR escalar
+    Vetor2d produto(float a);
+    // produto escalar
+    float produto(Vetor2d v1);
+};
+
+#endif // VETOR2D_H
+
+
+
+
+
+
+
