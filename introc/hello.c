a3230443d3286538bf93ae5e44de2032be1a7797
Agostinho <agostinhobritojr@gmail.com>
2024-02-28 08:39:15 -0300

    hello c

---
+++ introc/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // diretiva de compilacao
+
+int main(int argc, char *argv[]){
+  printf("alo, mundo!\n");
+  printf("argc = %d\n", argc);
+  printf("argv[1] = %s\n", argv[1]);
+}
+
