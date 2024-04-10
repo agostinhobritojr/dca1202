6dae40804990c2cb225306fb12ef8ad466d9d0a2
Agostinho <agostinhobritojr@gmail.com>
2024-04-10 08:32:59 -0300

    vetor2d

--- vetor2d/main.cpp
+++ vetor2d/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 class Vetor2d{
 private:
@@ -11,10 +12,29 @@ public:
     float getX(){
         return x;
     }
+    void setY(float y_){
+        y = y_;
+    }
+    float getY(){
+        return y;
+    }
+    float norma(){
+        return std::sqrt(x*x+y*y);
+    }
+    float angulo(){
+        return std::atan2(y,x)*180/M_PI;
+    }
+    void print(){
+        std::cout << "(" << x <<
+            ", " << y << ")\n";
+    }
 };
 
 int main(void){
     Vetor2d v;
 //    v.x = 4;
-    v.setX(3); std::cout << v.getX() << std::endl;
+    v.setX(3); v.setY(4);
+    v.print();
+    std::cout << "norma  = " << v.norma() << std::endl;
+    std::cout << "angulo = " << v.angulo() << std::endl;
 }
