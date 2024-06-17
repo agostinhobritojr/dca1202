10f2dab6592ffaeebadbffc543f1259b24aabe6c
Agostinho <agostinhobritojr@gmail.com>
2024-06-17 08:30:56 -0300

    .

---
+++ helloqt/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButtonQuit,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(finaliza()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::finaliza()
+{
+    exit(0);
+}
